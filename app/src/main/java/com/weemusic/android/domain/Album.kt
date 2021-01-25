be243977b5356b5299dff8eeeb2872578f213910
Hector Delgado <Hector.d.delgado@gmail.com>
2021-01-25 11:11:37 -0800

    Added convenience method to build Album from JsonObject

--- app/src/main/java/com/weemusic/android/domain/Album.kt
+++ app/src/main/java/com/weemusic/android/domain/Album.kt
@@ -1,5 +1,7 @@
 package com.weemusic.android.domain
 
+import android.util.Log
+import com.google.gson.JsonObject
 import org.threeten.bp.LocalDate
 
 data class Album(
@@ -10,5 +12,67 @@ data class Album(
     val title: String,
     val artist: String,
     val category: String,
-    val releaseDate: LocalDate
-)
+    val releaseDate: LocalDate,
+    val price: Double
+) {
+    companion object {
+        /**
+         * Creates an Album from a JsonObject using Gson.
+         *
+         * @param json The JsonObject that will be parsed.
+         *
+         * @return The Album created from the json data.
+         *
+         * @exception
+         */
+        fun from(json: JsonObject): Album {
+
+            val id = json
+                .getAsJsonObject("id")
+                .getAsJsonObject("attributes")
+                .getAsJsonPrimitive("im:id")
+                .asInt
+            val name = json
+                .getAsJsonObject("im:name")
+                .getAsJsonPrimitive("label")
+                .asString
+            val images = json
+                .getAsJsonArray("im:image")
+                .map {
+                    it.asJsonObject
+                        .getAsJsonPrimitive("label")
+                        .asString
+                }
+            val rights = json
+                .getAsJsonObject("rights")
+                .getAsJsonPrimitive("label")
+                .asString
+            val title = json
+                .getAsJsonObject("title")
+                .getAsJsonPrimitive("label")
+                .asString
+            val artist = json
+                .getAsJsonObject("im:artist")
+                .getAsJsonPrimitive("label")
+                .asString
+            val category = json
+                .getAsJsonObject("category")
+                .getAsJsonObject("attributes")
+                .getAsJsonPrimitive("label")
+                .asString
+            val releaseDate = LocalDate.parse(
+                json.getAsJsonObject("im:releaseDate")
+                    .getAsJsonPrimitive("label")
+                    .asString
+                    .substringBefore("T")
+            )
+            val price = json
+                .getAsJsonObject("im:price")
+                .getAsJsonObject("attributes")
+                .getAsJsonPrimitive("amount")
+                .asDouble
+
+            return Album(id, name, images, rights, title, artist, category, releaseDate, price)
+        }
+    }
+}
--- app/src/main/java/com/weemusic/android/ui/MainActivity.kt
+++ app/src/main/java/com/weemusic/android/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.weemusic.android.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
@@ -10,17 +11,20 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.JsonObject
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.squareup.picasso.Picasso
 import com.weemusic.android.R
 import com.weemusic.android.core.DaggerAppComponent
 import com.weemusic.android.core.DaggerDomainComponent
 import com.weemusic.android.core.DaggerNetworkComponent
+import com.weemusic.android.domain.Album
 import com.weemusic.android.domain.GetTopAlbumsUseCase
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import org.threeten.bp.LocalDate
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -44,6 +48,8 @@ class MainActivity : AppCompatActivity() {
             .domainComponent(domainComponent)
             .build()
             .inject(this)
+
+        AndroidThreeTen.init(this)
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -76,12 +82,34 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId) {
             R.id.sort_album -> {
+                val sortedAlbum = adapter.albums.sortedBy { albums ->
+                    albums.getAsJsonObject("im:name")
+                        .getAsJsonPrimitive("label")
+                        .asString
+                }
+                adapter = AlbumsAdapter(sortedAlbum)
+                rvFeed.adapter = adapter
                 true
             }
             R.id.sort_price -> {
+                val sortedAlbums = adapter.albums.sortedBy { albums ->
+                    albums.getAsJsonObject("im:price")
+                        .getAsJsonObject("attributes")
+                        .getAsJsonPrimitive("amount")
+                        .asDouble
+                }
+                adapter = AlbumsAdapter(sortedAlbums)
+                rvFeed.adapter = adapter
                 true
             }
             R.id.sort_title -> {
+                val sortedAlbums = adapter.albums.sortedBy { albums ->
+                    albums.getAsJsonObject("title")
+                        .getAsJsonPrimitive("label")
+                        .asString
+                }
+                adapter = AlbumsAdapter(sortedAlbums)
+                rvFeed.adapter = adapter
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -107,35 +135,20 @@ class MainActivity : AppCompatActivity() {
     class AlbumsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun onBind(album: JsonObject) {
-            val coverUrl = album
-                .getAsJsonArray("im:image")
-                .last()
-                .asJsonObject
-                .getAsJsonPrimitive("label")
-                .asString
-            val title = album
-                .getAsJsonObject("im:name")
-                .getAsJsonPrimitive("label")
-                .asString
-            val artist = album
-                .getAsJsonObject("im:artist")
-                .getAsJsonPrimitive("label")
-                .asString
-            val price = album
-                .getAsJsonObject("im:price")
-                .getAsJsonObject("attributes")
-                .getAsJsonPrimitive("amount")
-                .asDouble
+            val newAlbum = Album.from(album)
 
             val ivCover: ImageView = itemView.findViewById(R.id.ivCover)
             val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
             val tvArtist: TextView = itemView.findViewById(R.id.tvArtist)
             val tvPrice: TextView = itemView.findViewById(R.id.tvPrice)
+            val tvNew: TextView = itemView.findViewById(R.id.tvNew)
 
-            Picasso.with(itemView.context).load(coverUrl).into(ivCover)
-            tvTitle.text = title
-            tvArtist.text = artist
-            tvPrice.text = "$$price"
+            Picasso.with(itemView.context).load(newAlbum.images.last()).into(ivCover)
+            tvTitle.text = newAlbum.title
+            tvArtist.text = newAlbum.artist
+            tvPrice.text = "$${newAlbum.price}"
+            tvNew.visibility = if (LocalDate.now().toEpochDay() - newAlbum.releaseDate.toEpochDay() <= 60)
+                View.VISIBLE else View.GONE
         }
     }
 }
